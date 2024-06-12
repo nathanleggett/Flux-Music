5215f86f3c484a3cb1349d70b5297d8e293509af
nathanleggett <51215656+nathanleggett@users.noreply.github.com>
2024-06-12 23:01:57 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -33,8 +33,8 @@ module.exports = {
 
       const embed = new EmbedBuilder()
          .setColor('#0099ff')
-      .setTitle('💎 Prime Music Bot')
-      .setDescription('Welcome to the Music Bot!\n\n- Here are the available commands:\n\n' +
+      .setTitle('Flux Music Bot')
+      .setDescription('Welcome to the Flux Music Bot!\n\n- Here are the available commands:\n\n' +
         '**/play :** Start playing the songs.\n' +
         '**/ping :** check bot latency.\n' +
         '**/support :** Display support server info');
